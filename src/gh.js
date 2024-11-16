759c9e4d940ca5cf98fe63f8c194256ec2445a57
Dmitrii Iniutin <inyutin.da@gmail.com>
2024-11-16 16:36:06 +0000

    fix?2

--- src/gh.js
+++ src/gh.js
@@ -68,7 +68,7 @@ async function waitForRunnerRegistered(label) {
       if (waitSeconds > timeoutMinutes * 60) {
         core.error('GitHub self-hosted runner registration error');
         clearInterval(interval);
-        reject(`A timeout of ${timeoutMinutes} minutes is exceeded. Your AWS EC2 instance was not able to register itself in GitHub as a new self-hosted runner.`);
+        reject(`BA timeout of ${timeoutMinutes} minutes is exceeded. Your AWS EC2 instance was not able to register itself in GitHub as a new self-hosted runner.`);
       }
 
       if (runner && runner.status === 'idle') {
@@ -77,7 +77,7 @@ async function waitForRunnerRegistered(label) {
         resolve();
       } else {
         waitSeconds += retryIntervalSeconds;
-        core.info(`Checking... ${runner.name}`);
+        core.info(`Checking... ${runner.status}`);
       }
     }, retryIntervalSeconds * 1000);
   });
